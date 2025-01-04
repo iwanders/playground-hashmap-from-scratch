64e2417cb3aada7d3c87e40f58e1424bc2692cd5
Ivor Wanders <ivor@iwanders.net>
2025-01-04 15:52:34 -0500

    Also test with a larger map.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use criterion::{criterion_group, criterion_main, Criterion};
 use hashmap_from_scratch::BucketSeperateChainHashMap;
 use std::collections::HashMap;
@@ -6,25 +7,25 @@ use std::hint::black_box;
 // Hashbrown bench is at https://github.com/rust-lang/hashbrown/blob/master/benches/bench.rs
 
 macro_rules! default_benchmark {
-    ($name:ident, $maptype:ident, $label:expr) => {
+    ($name:ident, $maptype:ident, $label:expr, $cycles:expr, $count:expr) => {
         fn $name(b: &mut Criterion) {
             b.bench_function($label, |b| {
                 b.iter(|| {
                     let mut h = $maptype::new();
 
-                    // Each loop triggers one rehash
-                    for _ in 0..1000 {
-                        for i in 0..1000 {
+                    for _ in 0..$cycles {
+                        for i in 0..$count {
                             h.insert(i, i);
                         }
 
-                        assert_eq!(h.len(), 1000);
+                        assert_eq!(h.len(), $count);
 
-                        for i in 0..1000 {
+                        for i in 0..$count {
                             assert!(h.contains_key(&i));
                             assert!(h.get(&i).is_some());
+                            black_box(h.get(&i));
                         }
-                        for i in 0..1000 {
+                        for i in 0..$count {
                             h.remove(&i);
                         }
                         assert!(h.is_empty());
@@ -36,15 +37,43 @@ macro_rules! default_benchmark {
 }
 
 type StdHashmapU64U64 = HashMap<u64, u64>;
-default_benchmark!(criterion_std, StdHashmapU64U64, "std HashMap");
+default_benchmark!(
+    criterion_std_1k,
+    StdHashmapU64U64,
+    "std HashMap 1k",
+    1000,
+    1_000
+);
+default_benchmark!(
+    criterion_std_100k,
+    StdHashmapU64U64,
+    "std HashMap 100k",
+    1,
+    100_000
+);
 
 type BucketHashmapU64U64 = BucketSeperateChainHashMap<u64, u64>;
 default_benchmark!(
-    criterion_bucket_separate,
+    criterion_bucket_separate_1k,
     BucketHashmapU64U64,
-    "BucketSeparateCHain"
+    "BucketSeparateCHain 1k",
+    1000,
+    1_000
+);
+default_benchmark!(
+    criterion_bucket_separate_100k,
+    BucketHashmapU64U64,
+    "BucketSeparateCHain 100k",
+    1,
+    100_000
 );
 
-criterion_group!(benches, criterion_std, criterion_bucket_separate);
+criterion_group!(
+    benches,
+    criterion_std_1k,
+    criterion_std_100k,
+    criterion_bucket_separate_1k,
+    criterion_bucket_separate_100k,
+);
 
 criterion_main!(benches);
