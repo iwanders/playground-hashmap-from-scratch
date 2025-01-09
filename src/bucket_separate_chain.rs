ee5001fe70edb58b43b44eeaa7b15b4d867cf091
Ivor Wanders <ivor@iwanders.net>
2025-01-08 20:37:19 -0500

    Make the container generic over a bucket type container.

--- src/bucket_separate_chain.rs
+++ src/bucket_separate_chain.rs
@@ -6,38 +6,118 @@ impl<T: Hash + Eq> BucketKeyReq for T {}
 const DEFAULT_BUCKET_LOAD_FACTOR_MAX: f64 = 1.0;
 const DEFAULT_BUCKET_RESIZE_LOAD_FACTOR: f64 = 0.5;
 
+pub trait BucketInterface<K, V>: Sized {
+    fn len(&self) -> usize;
+
+    fn drain_into_map<M: HashMapInsertTrait<K, V>>(&mut self, map: &mut M);
+    fn vec_iter_mut<'a>(&'a mut self) -> impl std::iter::Iterator<Item = &'a mut (K, V)>
+    where
+        K: 'a,
+        V: 'a;
+    fn vec_iter<'a>(&'a self) -> impl std::iter::Iterator<Item = &'a (K, V)>
+    where
+        K: 'a,
+        V: 'a;
+    fn vec_swap_remove(&mut self, position: usize) -> (K, V);
+    fn vec_get(&self, index: usize) -> Option<&(K, V)>;
+    fn vec_push(&mut self, value: (K, V));
+}
+
+pub trait BucketContainerReq<K, V>:
+    std::ops::Index<usize> + BucketInterface<K, V> + Default
+{
+}
+impl<K, V, T: std::ops::Index<usize> + BucketInterface<K, V> + Default> BucketContainerReq<K, V>
+    for T
+{
+}
+
+impl<K: BucketKeyReq, V> BucketInterface<K, V> for Vec<(K, V)> {
+    fn len(&self) -> usize {
+        self.len()
+    }
+    fn drain_into_map<M: HashMapInsertTrait<K, V>>(&mut self, map: &mut M) {
+        for (k, v) in self.drain(..) {
+            map.map_insert(k, v);
+        }
+    }
+    fn vec_iter_mut<'a>(&'a mut self) -> impl std::iter::Iterator<Item = &'a mut (K, V)>
+    where
+        K: 'a,
+        V: 'a,
+    {
+        self.iter_mut()
+    }
+    fn vec_iter<'a>(&'a self) -> impl std::iter::Iterator<Item = &'a (K, V)>
+    where
+        K: 'a,
+        V: 'a,
+    {
+        self.iter()
+    }
+    fn vec_swap_remove(&mut self, position: usize) -> (K, V) {
+        self.swap_remove(position)
+    }
+    fn vec_get(&self, index: usize) -> Option<&(K, V)> {
+        self.get(index)
+    }
+    fn vec_push(&mut self, value: (K, V)) {
+        self.push(value);
+    }
+}
+
+pub trait HashMapInsertTrait<K, V> {
+    fn map_insert(&mut self, k: K, v: V);
+}
+
+impl<K: BucketKeyReq, V, BucketType: BucketContainerReq<K, V>> HashMapInsertTrait<K, V>
+    for BucketSeperateChainHashMap<K, V, BucketType>
+{
+    fn map_insert(&mut self, k: K, v: V) {
+        self.insert(k, v);
+    }
+}
+
+pub type HashmapChainVec<K, V> = BucketSeperateChainHashMap<K, V, Vec<(K, V)>>;
+
 #[derive(Debug)]
-pub struct BucketSeperateChainHashMap<K: BucketKeyReq, V> {
+pub struct BucketSeperateChainHashMap<K: BucketKeyReq, V, BucketType: BucketContainerReq<K, V>> {
     entries: usize,
     load_factor_max: f64,
     resize_load_factor: f64,
-    // low hanging fruit is to make this a smallvec of 1, such that if there's no hash collition it's nice
-    // and colocated in the main vector. The branch in-bucket-head has that implemented and that
-    // brings the small 1k test within a percent of std::collection::Hashmap
-    buckets: Vec<Vec<(K, V)>>,
+    buckets: Vec<BucketType>,
+    _z: std::marker::PhantomData<(K, V)>,
 }
-impl<K: BucketKeyReq, V> Default for BucketSeperateChainHashMap<K, V> {
+impl<K: BucketKeyReq, V, BucketType: BucketContainerReq<K, V>> Default
+    for BucketSeperateChainHashMap<K, V, BucketType>
+{
     fn default() -> Self {
         Self {
             entries: 0,
             load_factor_max: DEFAULT_BUCKET_LOAD_FACTOR_MAX,
             resize_load_factor: DEFAULT_BUCKET_RESIZE_LOAD_FACTOR,
-            buckets: vec![Default::default()],
+            buckets: vec![BucketType::default()],
+            _z: Default::default(),
         }
     }
 }
-impl<K: BucketKeyReq + Clone, V: Clone> Clone for BucketSeperateChainHashMap<K, V> {
+impl<K: BucketKeyReq + Clone, V: Clone, BucketType: BucketContainerReq<K, V> + Clone> Clone
+    for BucketSeperateChainHashMap<K, V, BucketType>
+{
     fn clone(&self) -> Self {
         Self {
             entries: self.entries,
             load_factor_max: self.load_factor_max,
             resize_load_factor: self.resize_load_factor,
             buckets: self.buckets.clone(),
+            _z: Default::default(),
         }
     }
 }
 
-impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
+impl<K: BucketKeyReq, V, BucketType: BucketContainerReq<K, V>>
+    BucketSeperateChainHashMap<K, V, BucketType>
+{
     fn calculate_bucket_index(&self, k: &K) -> usize {
         // First calculate the hash.
         let mut hasher = std::hash::DefaultHasher::new();
@@ -60,9 +140,7 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
 
         // Drain self into the new map.
         for mut v in self.buckets.drain(..) {
-            for (k, v) in v.drain(..) {
-                new_map.insert(k, v);
-            }
+            v.drain_into_map(&mut new_map)
         }
 
         // Replace the map.
@@ -78,9 +156,9 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
         println!(" load: {load}");
         println!(" buckets: {}", self.buckets.len());
         println!(" entries: {}", self.entries);
-        for (i, b) in self.buckets.iter().enumerate() {
-            println!(" b[{i}]: {}", b.len());
-        }
+        // for (i, b) in self.buckets.iter().enumerate() {
+        // println!(" b[{i}]: {}", b.len());
+        // }
     }
 
     pub fn load_factor_max(&self) -> f64 {
@@ -98,7 +176,9 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
 }
 
 // Use this block to hold the 'std' methods.
-impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
+impl<K: BucketKeyReq, V, BucketType: BucketContainerReq<K, V>>
+    BucketSeperateChainHashMap<K, V, BucketType>
+{
     /// Create a new hashmap.
     pub fn new() -> Self {
         Self::default()
@@ -136,19 +216,21 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
     /// Insert a key.
     pub fn insert(&mut self, key: K, value: V) {
         let bucket_index = self.calculate_bucket_index(&key);
+
         // We found the bucket.
         let b: &mut _ = &mut self.buckets[bucket_index];
         // In that bucket we may already have the key, so search for it if so update it.
-        for (bk, bv) in b.iter_mut() {
+        for (bk, bv) in b.vec_iter_mut() {
             if *bk == key {
                 *bv = value;
                 return;
             }
         }
 
+        let b: &mut _ = &mut self.buckets[bucket_index];
         // We did not find this key already, so we append it to the bucket.
         self.entries += 1;
-        b.push((key, value));
+        b.vec_push((key, value));
 
         // Resize if that was actually necessary
         self.resize_to(self.entries);
@@ -160,11 +242,12 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
         // We found the bucket.
         let b: &_ = &self.buckets[bucket_index];
         // Search in that bucket.
-        for (bk, _bv) in b.iter() {
+        for (bk, _bv) in b.vec_iter() {
             if *bk == *k {
                 return true;
             }
         }
+
         false
     }
 
@@ -181,6 +264,20 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
     /// Remove an entry from the hashmap.
     pub fn remove(&mut self, key: &K) -> Option<V> {
         let bucket_index = self.calculate_bucket_index(&key);
+
+        // Why does our implementation need an intermediate, but the 'real' one doesn't?
+        let intermediate = self.buckets[bucket_index]
+            .vec_iter()
+            .position(|(bk, _)| *bk == *key);
+
+        if let Some(index_in_bucket) = intermediate {
+            let v = self.buckets[bucket_index].vec_swap_remove(index_in_bucket);
+            self.entries -= 1;
+            return Some(v.1);
+        } else {
+            return None;
+        };
+        /*
         if let Some(index_in_bucket) = self.buckets[bucket_index]
             .iter()
             .position(|(bk, _)| *bk == *key)
@@ -191,11 +288,24 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
         } else {
             None
         }
+        */
     }
 
     /// Get a value by reference.
     pub fn get(&self, key: &K) -> Option<&V> {
         let bucket_index = self.calculate_bucket_index(&key);
+
+        let intermediate = self.buckets[bucket_index]
+            .vec_iter()
+            .position(|(bk, _)| *bk == *key);
+        if let Some(index_in_bucket) = intermediate {
+            self.buckets[bucket_index]
+                .vec_get(index_in_bucket)
+                .map(|(_, v)| v)
+        } else {
+            None
+        }
+        /*
         if let Some(index_in_bucket) = self.buckets[bucket_index]
             .iter()
             .position(|(bk, _)| *bk == *key)
@@ -205,7 +315,7 @@ impl<K: BucketKeyReq, V> BucketSeperateChainHashMap<K, V> {
                 .map(|(_, v)| v)
         } else {
             None
-        }
+        }*/
     }
 }
 
@@ -214,7 +324,7 @@ mod test {
     use super::*;
     #[test]
     fn test_bucket_seperate_chain() {
-        let mut h = BucketSeperateChainHashMap::<u64, u64>::new();
+        let mut h = HashmapChainVec::<u64, u64>::new();
         h.insert(300, 3);
         h.insert(500, 8);
         assert!(h.contains_key(&300));
@@ -234,9 +344,12 @@ mod test {
         let z = h.clone();
         assert!(!z.contains_key(&300));
         assert_eq!(z.len(), 33);
+    }
 
+    #[test]
+    fn test_bucket_seperate_chain_nonclone() {
         struct NonClone {}
-        let non_clonable = BucketSeperateChainHashMap::<u64, NonClone>::new();
+        let non_clonable = HashmapChainVec::<u64, NonClone>::new();
         // let z = non_clonable.clone();
         let _ = non_clonable;
     }
@@ -245,7 +358,7 @@ mod test {
     fn test_fuzz() {
         use rand::prelude::*;
         let mut rng = rand::thread_rng();
-        let mut h = BucketSeperateChainHashMap::<u64, u64>::new();
+        let mut h = HashmapChainVec::<u64, u64>::new();
         let mut r = std::collections::HashMap::<u64, u64>::new();
 
         // Insert 10000 into both.
