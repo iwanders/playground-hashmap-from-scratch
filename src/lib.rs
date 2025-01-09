6441100d158fc69a804d7ba6634a2a1498901782
Ivor Wanders <ivor@iwanders.net>
2025-01-08 20:38:42 -0500

    Reinstate the simple version in a separate file.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod bucket_separate_chain;
 
 pub use bucket_separate_chain::BucketSeperateChainHashMap;
 
+pub mod bucket_seperate_chain_simple;
+
 pub type MainError = Box<dyn std::error::Error + Sync + Send>;
 pub fn main() -> Result<(), MainError> {
     Ok(())
